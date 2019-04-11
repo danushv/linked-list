bca0dd21b1bf03cd62f303a0bac2cf2b4293141a
danny boi <43886538+danushv@users.noreply.github.com>
2019-04-11 21:11:30 +0530

    Add files via upload

---
+++ linkedlist1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct Node
+{ 
+int data;
+struct Node *next;
+};
+void printList(struct Node *n)
+{ 
+while(n!=NULL)
+{
+	cout<<n->data;
+	n=n->next;
+}
+}
+int main() 
+{ 
+struct Node * head =NULL;
+struct Node * second =NULL;
+struct Node *third =NULL;
+
+head=(struct Node*)malloc(sizeof(struct Node));
+second =(struct Node *)malloc(sizeof(struct Node));
+third =(struct Node*)malloc(sizeof(struct Node));
+head->data=1;
+head -> next =second;
+second ->data=2;
+second->next=third;
+third ->data=3;
+third ->next =NULL;
+printList(second);
+}
